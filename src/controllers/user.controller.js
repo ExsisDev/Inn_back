915112975651d2dc1a6f6b39ec7d28688295bcd9
Juan Castelblanco <jscastelblancoh@unal.edu.co>
2019-12-01 11:28:39 -0500

    Token fixed. Push to azure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -202,7 +202,7 @@ function authenticateUser(res, userAttributes) {
       await updateHour(userAttributes.user_email, DateTime.local().setZone('America/Bogota'));
       await updateLoginCounter(userAttributes.user_email, 0);
       token = userAuthenticated.generateAuthToken();
-      return res.set('x-auth-token', token).send("Usuario autenticado");
+      return res.set('x-auth-token', token).set('Access-Control-Expose-Headers', 'x-auth-token').send("Usuario autenticado");
 
    });
 }
